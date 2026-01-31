1d11a3a1cc8fd9c3556dff868235ec12456c386a
Arif14377 <arifrahman14377@gmail.com>
2026-01-31 23:59:20 +0700

    feat: create login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,12 @@
 import image from "../assets/images/register.png"
 import brandLogo from "../assets/images/brand-brown.png"
 import InputAuth from "../components/InputAuth"
-import { BsPerson } from "react-icons/bs";
 import Button from "../components/Button";
-import { handleRegister } from "../Auth/register";
+import { handleLogin } from "../Auth/login";
 import { AiOutlineMail } from "react-icons/ai";
 import { MdKey } from "react-icons/md";
 
-export function Register() {
+export function Login() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 min-h-screen">
             <div className="hidden md:block md:col-span-1">
@@ -16,16 +15,14 @@ export function Register() {
             <div className="flex items-center justify-center md:col-span-2">
                 <div className="w-4/5 md:w-3/5 p-auto flex flex-col gap-4">
                     <img src={brandLogo} alt="logo coffee shop" className="w-fit"/>
-                    <h1 className="text-2xl font-medium text-shadow-orange-300">Register</h1>
+                    <h1 className="text-2xl font-medium text-shadow-orange-300">Login</h1>
                     <p>Fill out the form correctly</p>
-                    <form typeof="submit" className="flex flex-col gap-4" onSubmit={handleRegister}>
-                        <InputAuth type={"text"} id={"name"} label={"Name"} name={"name"} iconInput={<BsPerson />} placeholder={"Enter Your Full Name"}/>
+                    <form typeof="submit" className="flex flex-col gap-4" onSubmit={handleLogin}>
                         <InputAuth type={"email"} id={"email"} label={"Email"} name={"email"} iconInput={<AiOutlineMail />} placeholder={"Enter Your Email"}/>
                         <InputAuth type={"password"} id={"password"} label={"Password"} name={"password"} iconInput={<MdKey className="border rounded" />} placeholder={"Enter Your Password"}/>
-                        <InputAuth type={"password"} id={"verifPassword"} label={"Confirm Password"} iconInput={<MdKey className="border rounded" />} placeholder={"Enter Your Password Again"}/>
-                        <Button label={"Register"} type={"submit"} variant={"primary"} className={""}/>
+                        <Button label={"Login"} type={"submit"} variant={"primary"} className={""}/>
                     </form>
-                    <p className="flex justify-center gap-2">Have An Account? <a className="text-orange-500" href="#">Login</a></p>
+                    <p className="flex justify-center gap-2">Not Have An Account? <a className="text-orange-500" href="#">Register</a></p>
                 </div>
             </div>
         </div>
